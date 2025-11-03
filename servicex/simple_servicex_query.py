b2c5e701246825554d592665dbcbd86d004af5ad
Oksana Shadura <ksu.shadura@gmail.com>
2025-11-03 11:23:28 +0100

    Add simple servicex test (will not work nopw because of broken BEARER_TOKEN_FILE)

---
+++ servicex/simple_servicex_query.py
@@ -0,0 +1,26 @@
+from servicex import query, dataset, deliver
+
+
+spec = {
+    "Sample": [
+        {
+            "Name": "UprootRaw_Dictcms1",
+            "Dataset": dataset.FileList(
+                [
+                    #"root://xcache.cmsaf-dev.flatiron.hollandhpc.org//store/mc/RunIISummer20UL18NanoAODv9/DYJetsToLL_M-50_HT-100to200_TuneCP5_PSweights_13TeV-madgraphMLM-pythia8/NANOAODSIM/106X_upgrade2018_realistic_v16_L1v1-v1/70000/92DF66B4-2A9E-4A49-8B43-A1B2F4B24379.root",  # noqa: E501
+                    "root://cmsxrootd.fnal.gov//store/mc/RunIISummer20UL18NanoAODv9/DYJetsToLL_M-50_HT-100to200_TuneCP5_PSweights_13TeV-madgraphMLM-pythia8/NANOAODSIM/106X_upgrade2018_realistic_v16_L1v1-v1/70000/92DF66B4-2A9E-4A49-8B43-A1B2F4B24379.root"
+                ]
+            ),
+            "Query": query.UprootRaw(
+                [
+                    {
+                        "treename": "Events",
+                        "filter_name": "MET_pt",
+                    }
+                ]
+            ),
+        }
+    ]
+}
+
+print(f"Files: {deliver(spec)}")
